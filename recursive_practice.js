1a787029a5f05e9645f0c9ae5e07863d34308ada
Isaac Granillo <isaac.granillo@gmail.com>
2016-03-30 17:44:41 -0700

    added recursive_practice.js

---
+++ recursive_practice.js
@@ -0,0 +1,17 @@
+function add(num){
+	if(num < 1){
+		return 0;
+	};
+	return num + add(num - 1)
+};
+
+
+function fib(num){
+	if(num < 1){
+		return 0;
+	};
+	if(num < 2){
+		return 1;
+	};
+	return fib(num - 1) + fib(num - 2);
+};
