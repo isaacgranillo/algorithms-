a1c614ed94d742bc013ccd1b5eec43218fb23e1c
Isaac Granillo <isaac.granillo@gmail.com>
2016-04-15 17:27:40 -0700

    added balanced_brackets

---
+++ balanced_bracket.js
@@ -0,0 +1,41 @@
+//uses a stack
+
+// var left_brackets = ["(", "[", "{"];
+// var right_brackets = [")", "]", "}"]
+var right_to_left = {
+		")" : "(",
+		"]" : "[",
+		"}" : "{"
+	}
+
+
+function balancedBracket(str){
+
+	var stack = [];
+
+	//iterate through string and push left brackets
+	for(var i = 0; i < str.length; i++){
+		if(str[i] === "(" || str[i] === "[" || str[i] === "{"){
+			stack.push(str[i])
+		}
+		//compare left to right and pop 
+		else if(str[i] === ")" || str[i] === "]" || str[i] === "}"){
+			if(!stack || stack.pop() != right_to_left[str[i]]){
+				return false
+			}
+		}
+
+	}
+	
+	//if any remaining characters left return false
+	if(stack.length > 0){
+			return false
+		}
+
+	//all characters have a correct match and stack is now empty
+	return true
+}
+
+console.log(balancedBracket("{}[()]((){})"))
+
+
